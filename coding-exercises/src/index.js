0fa85cdad7b401314f968fd7d797778942646bf4
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-02 02:42:35 +0530

    starter-files

---
+++ coding-exercises/src/index.js
@@ -0,0 +1,39 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return;
+}
+
+function Intro() {
+  return;
+}
+
+function SkillList() {
+  return;
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
