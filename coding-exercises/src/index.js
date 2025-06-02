79d3c0fce3d8bba80d4acaf5e1e714621d9c9aaa
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-02 22:03:13 +0530

    exercise-1

--- coding-exercises/src/index.js
+++ coding-exercises/src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar photoName="sahil.jpeg" name="sahil" />
       <div className="data">
         <Intro />
         {/* Should contain one Skill component
@@ -17,16 +17,40 @@ function App() {
   );
 }
 
-function Avatar() {
-  return;
+function Avatar(props) {
+  console.log(props);
+  return <img src={props.photoName} alt={props.name} className="avatar" />;
 }
 
 function Intro() {
-  return;
+  return (
+    <div>
+      <h1>Sahil Hussain</h1>
+      <p>
+        Pursuing B.Tech CSE at The Assam Royal Global University. Learning React
+        from Jonas Schmedtmann. Skills include:
+      </p>
+    </div>
+  );
 }
 
 function SkillList() {
-  return;
+  return (
+    <div className="skill-list">
+      <Skill skill="C/C++" color="MediumSeaGreen" />
+      <Skill skill="HTML+CSS" color="orangered" />
+      <Skill skill="JavaScript" color="yellow" />
+      <Skill skill="React" color="DodgerBlue" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+    </div>
+  );
 }
 
 const rootElement = document.getElementById("root");
