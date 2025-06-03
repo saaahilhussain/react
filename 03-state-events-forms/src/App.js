a1afd20b8af25fcc64751d65120c13efffb1a88c
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-03 22:06:01 +0530

    a card component

---
+++ 03-state-events-forms/src/App.js
@@ -0,0 +1,26 @@
+const messages = ["Learn React ⚛️", "Make Projects 🛠️", "Apply for jobs 💼"];
+
+export default function App() {
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
