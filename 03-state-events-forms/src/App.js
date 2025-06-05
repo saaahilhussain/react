9a6a5258b7d24a25cec5c30a1339af86e4405f70
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-05 15:22:36 +0530

    impl: state

--- 03-state-events-forms/src/App.js
+++ 03-state-events-forms/src/App.js
@@ -1,21 +1,23 @@
+import { useState } from "react";
+
 const messages = ["Learn React ⚛️", "Make Projects 🛠️", "Apply for jobs 💼"];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
   }
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
