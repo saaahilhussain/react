933d88ca62907bb4a60798ea681475e80694e27c
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-05 21:55:52 +0530

    exercise-1

--- 03-state-events-forms/src/App.js
+++ 03-state-events-forms/src/App.js
@@ -3,6 +3,15 @@ import { useState } from "react";
 const messages = ["Learn React ⚛️", "Make Projects 🛠️", "Apply for jobs 💼"];
 
 export default function App() {
+  return (
+    <div>
+      <LearnState />
+      <Exercise1 />
+    </div>
+  );
+}
+
+function LearnState() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -47,5 +56,39 @@ export default function App() {
         </div>
       )}
     </>
+  );
+}
+
+function Exercise1() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span> step: {step} </span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span> count: {count} </span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
   );
 }
